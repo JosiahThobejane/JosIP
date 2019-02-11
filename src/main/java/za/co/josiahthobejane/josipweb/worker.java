479d1c171b0abd9c04d1365f98627052bf1326d1
Josiah Thobejane <33384308+JosiahThobejane@users.noreply.github.com>
2019-02-11 16:23:33 +0200

    clean

--- src/main/java/za/co/josiahthobejane/josipweb/worker.java
+++ src/main/java/za/co/josiahthobejane/josipweb/worker.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package za.co.josiahthobejane.josipweb;
+package za.co.josiahthobejane.josip;
 
 import java.io.IOException;
 import java.io.PrintWriter;
